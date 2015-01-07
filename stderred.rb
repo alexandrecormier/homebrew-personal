d1baab894241a7757a456eadcf3205f272fa0054
Alexandre Cormier <alexandre@cormier.io>
2015-01-07 00:44:06 -0500

    Add stderred

---
+++ stderred.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Stderred < Formula
+    homepage 'https://github.com/sickill/stderred'
+    head 'https://github.com/sickill/stderred.git'
+
+    depends_on 'cmake' => :build
+
+    option :universal
+
+    def install
+        ENV.universal_binary if build.universal?
+        mkdir 'build' do
+            system "cmake", '../src', *std_cmake_args
+            system 'make', 'install'
+        end
+    end
+
+    def caveats; <<-EOS.undent
+        To use stderred add the following to your .bashrc/.zshrc:
+            brew_prefix="$(brew --prefix)"
+            stderr_binary="$brew_prefix/lib/libstderred.dylib"
+            [ -f $stderred_binary ] && export DYLD_INSERT_LIBRARIES="$stderred_binary${DYLD_INSERT_LIBRARIES:+:DYLD_INSERT_LIBRARIES}"
+        EOS
+    end
+end
